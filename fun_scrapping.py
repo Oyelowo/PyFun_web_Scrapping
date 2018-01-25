0a7e835d6937e4942a1c27c07c6f45dd09debc90
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-01-25 16:03:09 +0200

    publication dates and character counts included

--- fun_scrapping.py
+++ fun_scrapping.py
@@ -13,7 +13,8 @@ import pandas as pd
 
 #get the url. 
 #I have decided to try YLE which is a news website in Finland"
-#I am curious to know how many times name_of_interest has appeared in the news.
+#I am curious to know how many times a name has appeared in the news.
+#What was the content pictures used e.tc? Perharps. 
 
 #get the page for rss feeds.
 url='https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss'
@@ -80,15 +81,24 @@ for i, item in enumerate(items):
     news_items.loc[i, 'description']=item.description.text
 #    news_items.loc[i, 'description_en']=(tr.translate(item.description.text)).text
     news_items.loc[i, 'image']=item.enclosure['url'] if item.enclosure is not None else ''
-    content_xml=item.find('content:encoded').text
-    content=''.join(bs(content_xml, "lxml").findAll(text=True))
+#    publication date
+    news_items.loc[i, 'pubDate']=item.pubDate.text
+    if item.find('content:encoded') is not None:
+        #get the content of each article
+        content_xml=item.find('content:encoded').text
+        content=''.join(bs(content_xml, "lxml").findAll(text=True))
     #    this  gets the body, removes the extra characters to leave plain text
-    news_items.loc[i, 'content']= content
-#    news_items.loc[i, 'body_en']= (tr.translate(content)).text
+        news_items.loc[i, 'content']= content
+    #length of article
+        news_items.loc[i, 'character']=len(content)
+        news_items.loc[i, 'word_count']=len(content.split())
+        news_items.loc[i, 'sentence']=len(content.split('.'))
 
+#export the data frame
+#news_items.to_csv(r'C:\Users\oyeda\Desktop\PYFUN\news_scrape.txt', sep=';')
 
 #name to be explored in the rss feeds of the url.
-name_of_interest='president'
+name_of_interest='Helsinki'
 
 # e.g: we can see how many times name_of_interest appeared in the news
 name_of_interest_occur=len([rows for idx,rows in news_items.iterrows() if name_of_interest in rows.title])
@@ -145,7 +155,7 @@ print("There are {number} pictures used in all {name}'s publications.".format(nu
 
 #now, let's see the pictures.
 #first picture
-name_of_interest_pics[3]
+name_of_interest_pics[1]
     
 
 #second picture
